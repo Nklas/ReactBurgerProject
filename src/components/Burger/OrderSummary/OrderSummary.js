08df14a97607fcea95503cd248a3737c734cf649
Nklas <niklaus462@gmail.com>
2019-07-06 15:29:23 +0300

    add burger project

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+
+import Au from '../../../hoc/Au';
+import Button from '../../UI/Button/Button';
+
+class OrderSummary extends Component {
+
+    componentWillUpdate() {
+        console.log('[OrderSummary] WillUpdate');
+    }
+
+    render () {
+
+        const ingredientSummary = Object.keys(this.props.ingredients)
+        .map(igKey => {
+            return (
+                <li key={igKey}>
+                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+                </li>);
+        });
+
+        return (
+            <Au>
+            <h3>Your Order</h3>
+            <p>A delicious burger with the following ingredients:</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p><strong>Total price: {this.props.price.toFixed(2)}</strong></p>
+            <p>Continue to chechout?</p>
+            <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+        </Au>
+        );
+    }
+}
+
+export default OrderSummary;
---
+++ src/hoc/Au.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const au = (props) => props.children;
+
+export default au;
